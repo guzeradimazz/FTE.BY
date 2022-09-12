b94cfc4c594f0b231dcaaac6dba3a21b247ce10c
guzeradimazz <Guzerashop@mail.ru>
2022-09-13 02:33:27 +0300

    add modal

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -16,3 +16,21 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = 'block'
     evt.currentTarget.className += ' active'
 }
+
+
+const modal = document.getElementById("myModal");
+
+const btn = document.getElementById("myBtn");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
